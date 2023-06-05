55dd61450aea6d407dd49e901869380f7a3afce3
Amartya <amartyahatua@gmail.com>
2023-06-04 22:40:22 -0400

    'updating_XGB'

--- code/ml_model/xgb.py
+++ code/ml_model/xgb.py
@@ -1,19 +1,28 @@
-import os
-import  pandas as pd
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import recall_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
-import arraygan.git.code.ml_model.dataconfig as config
-import spacy_universal_sentence_encoder
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.metrics import classification_report
-from sentence_transformers import SentenceTransformer
+import os
 import pickle
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.decomposition import IncrementalPCA
+from sklearn.metrics import classification_report
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 
 
+# load one of the models: ['en_use_md', 'en_use_lg', 'xx_use_md', 'xx_use_lg']
+# Universal sentence encoder
+# nlp = spacy_universal_sentence_encoder.load_model('en_use_lg')
 
+# BERT Sentence encoder
+# nlp = SentenceTransformer('all-MiniLM-L6-v2')
+
+# get two documents
+# doc_1 =
+
+# sbert_model = spacy_universal_sentence_encoder('paraphrase-distilroberta-base-v1')
 
 def apply_pca(data):
     pca = IncrementalPCA(n_components=128)
@@ -21,92 +30,188 @@ def apply_pca(data):
     return principalComponents
 
 
+sets = 5
+processes = 3
+
+data_path_clean = '../../data/clean'
+data_path_preprocess = '../../data/preprocessing'
+
+col_name = [['Set', 'Process',\
+            'GT Precision', 'GT Recall', 'GT F1',\
+            'Clean Precision', 'Clean Recall', 'Clean F1',\
+            'Clean No Sp Precision', 'Clean No Sp Recall', 'Clean No Sp F1',\
+            'Clean Sp Precision', 'Clean Sp Recall', 'Clean Sp F1']]
+
+
 def XGB():
-    # Universal sentence encoder
-    # nlp = spacy_universal_sentence_encoder.load_model('en_use_lg')
+    final_result = pd.DataFrame()
+    for turn in range(1):
+        for set in range(sets):
+            for process in range(processes):
+                result = []
+                result.append(process + 1)
+                result.append(set + 1)
+                df_train = pd.read_csv(
+                    os.path.join(data_path_clean, 'fever_train', 'FEVER_Train_{}_{}.csv'.format(set + 1, process + 1)))
+                df_test_1 = pd.read_csv(os.path.join(data_path_clean, 'fever_1_test',
+                                                     'FEVER_1_Test_{}_{}.csv'.format(set + 1, process + 1)))
+                df_test_2 = pd.read_csv(os.path.join(data_path_clean, 'fever_2_test',
+                                                     'FEVER_2_Test_{}_{}.csv'.format(set + 1, process + 1)))
 
-    # BERT Sentence encoder
-    nlp = SentenceTransformer('all-MiniLM-L6-v2')
-    for turn in range(5):
-        for proecess in range(1,15):
-            # arraygan / model / preprocess_data / set_1 / data.csv
-            df = pd.read_csv('../../../model/preprocess_data/set_{}/data.csv'.format(str(proecess)))
-            df_test = pd.read_csv('../../../model/preprocess_data/set_{}/df_test_1.csv'.format(str(proecess)))
+                df_claims = df_train['claims']
+                df_claims_clean = df_train['claims_clean']
+                df_claims_clean_no_space = df_train['cleaning_no_space']
+                df_claims_clean_space = df_train['cleaning_space']
+                df_classes = df_train['class']
 
-            df_claim = df['claims']
-            df_class = df['classes']
+                df_test_1_claims = df_test_1['claims']
+                df_test_1_claims_clean = df_test_1['claims_clean']
+                df_test_1_claims_clean_no_space = df_test_1['cleaning_no_space']
+                df_test_1_claims_clean_space = df_test_1['cleaning_space']
+                df_test_1_classes = df_test_1['classes']
 
-            df_claim = df_claim.values.tolist()
-            df_class = df_class.values.tolist()
+                df_test_2_claims = df_test_2['claims']
+                df_test_2_claims_clean = df_test_2['claims_clean']
+                df_test_2_claims_clean_no_space = df_test_2['cleaning_no_space']
+                df_test_2_claims_clean_space = df_test_2['cleaning_space']
+                df_test_2_classes = df_test_2['classes']
 
-            X_test = df_test['claims']
-            y_test = df_test['classes']
+                # Ground truth
+                tfIdfVectorizer = TfidfVectorizer(lowercase=False)
+
+                tfidf_train_vectors = tfIdfVectorizer.fit_transform(df_claims)
+                tfidf_test_vectors_1 = tfIdfVectorizer.transform(df_test_1_claims)
+                tfidf_test_vectors_2 = tfIdfVectorizer.transform(df_test_2_claims)
+
+                clf = GradientBoostingClassifier(learning_rate=0.01, n_estimators=1000)
+                model = clf.fit(tfidf_train_vectors, df_classes)
+                filename = '../model/TFIDF_xgb_model_process_{}_turn_{}_GT.pkl'.format(str(set + 1),
+                                                                                       str(process + 1))
+                pickle.dump(model, open(filename, 'wb'))
+                predicted_fever_1 = model.predict(tfidf_test_vectors_1)
+                predicted_fever_2 = model.predict(tfidf_test_vectors_2)
+
+                classification_score_test_1 = classification_report(df_test_1_classes, predicted_fever_1, digits=4)
+                precision_test_1 = classification_score_test_1.split('\n')[-2].split()[2:-1][0]
+                recall_test_1 = classification_score_test_1.split('\n')[-2].split()[2:-1][1]
+                f1_score_test_1 = classification_score_test_1.split('\n')[-2].split()[2:-1][2]
+
+                result.append(precision_test_1)
+                result.append(recall_test_1)
+                result.append(f1_score_test_1)
+
+                classification_score_test_2 = classification_report(df_test_2_classes, predicted_fever_2, digits=4)
+                precision_test_2 = classification_score_test_2.split('\n')[-2].split()[2:-1][0]
+                recall_test_2 = classification_score_test_2.split('\n')[-2].split()[2:-1][1]
+                f1_score_test_2 = classification_score_test_2.split('\n')[-2].split()[2:-1][2]
+
+                result.append(precision_test_2)
+                result.append(recall_test_2)
+                result.append(f1_score_test_2)
+
+                # Clean data
+                tfIdfVectorizer = TfidfVectorizer(lowercase=False)
+
+                tfidf_train_vectors = tfIdfVectorizer.fit_transform(df_claims_clean)
+                tfidf_test_vectors_1 = tfIdfVectorizer.transform(df_test_1_claims_clean)
+                tfidf_test_vectors_2 = tfIdfVectorizer.transform(df_test_2_claims_clean)
+
+                clf = GradientBoostingClassifier(learning_rate=0.01, n_estimators=1000)
+                model = clf.fit(tfidf_train_vectors, df_classes)
+                predicted_fever_1 = model.predict(tfidf_test_vectors_1)
+                predicted_fever_2 = model.predict(tfidf_test_vectors_2)
+
+                filename = '../model/TFIDF_xgb_model_process_{}_turn_{}_Cleaning.pkl'.format(str(set + 1),
+                                                                                             str(process + 1))
+                pickle.dump(model, open(filename, 'wb'))
+
+                classification_score_test_1 = classification_report(df_test_1_classes, predicted_fever_1, digits=4)
+                precision_test_1 = classification_score_test_1.split('\n')[-2].split()[2:-1][0]
+                recall_test_1 = classification_score_test_1.split('\n')[-2].split()[2:-1][1]
+                f1_score_test_1 = classification_score_test_1.split('\n')[-2].split()[2:-1][2]
+
+                result.append(precision_test_1)
+                result.append(recall_test_1)
+                result.append(f1_score_test_1)
+
+                classification_score_test_2 = classification_report(df_test_2_classes, predicted_fever_2, digits=4)
+                precision_test_2 = classification_score_test_2.split('\n')[-2].split()[2:-1][0]
+                recall_test_2 = classification_score_test_2.split('\n')[-2].split()[2:-1][1]
+                f1_score_test_2 = classification_score_test_2.split('\n')[-2].split()[2:-1][2]
+
+                result.append(precision_test_2)
+                result.append(recall_test_2)
+                result.append(f1_score_test_2)
+
+                # Clean no space data
+                tfIdfVectorizer = TfidfVectorizer(lowercase=False)
+
+                tfidf_train_vectors = tfIdfVectorizer.fit_transform(df_claims_clean_no_space)
+                tfidf_test_vectors_1 = tfIdfVectorizer.transform(df_test_1_claims_clean_no_space)
+                tfidf_test_vectors_2 = tfIdfVectorizer.transform(df_test_2_claims_clean_no_space)
+
+                clf = GradientBoostingClassifier(learning_rate=0.01, n_estimators=1000)
+                model = clf.fit(tfidf_train_vectors, df_classes)
+                predicted_fever_1 = model.predict(tfidf_test_vectors_1)
+                predicted_fever_2 = model.predict(tfidf_test_vectors_2)
+
+                filename = '../model/TFIDF_xgb_model_process_{}_turn_{}_Clean_No_Space.pkl'.format(str(set + 1),
+                                                                                                   str(process + 1))
+                pickle.dump(model, open(filename, 'wb'))
+
+                classification_score_test_1 = classification_report(df_test_1_classes, predicted_fever_1, digits=4)
+                precision_test_1 = classification_score_test_1.split('\n')[-2].split()[2:-1][0]
+                recall_test_1 = classification_score_test_1.split('\n')[-2].split()[2:-1][1]
+                f1_score_test_1 = classification_score_test_1.split('\n')[-2].split()[2:-1][2]
+
+                result.append(precision_test_1)
+                result.append(recall_test_1)
+                result.append(f1_score_test_1)
+
+                classification_score_test_2 = classification_report(df_test_2_classes, predicted_fever_2, digits=4)
+                precision_test_2 = classification_score_test_2.split('\n')[-2].split()[2:-1][0]
+                recall_test_2 = classification_score_test_2.split('\n')[-2].split()[2:-1][1]
+                f1_score_test_2 = classification_score_test_2.split('\n')[-2].split()[2:-1][2]
+
+                result.append(precision_test_2)
+                result.append(recall_test_2)
+                result.append(f1_score_test_2)
+
+                # Clean with space data
+                tfIdfVectorizer = TfidfVectorizer(lowercase=False)
+
+                tfidf_train_vectors = tfIdfVectorizer.fit_transform(df_claims_clean_space)
+                tfidf_test_vectors_1 = tfIdfVectorizer.transform(df_test_1_claims_clean_space)
+                tfidf_test_vectors_2 = tfIdfVectorizer.transform(df_test_2_claims_clean_space)
+
+                clf = GradientBoostingClassifier(learning_rate=0.01, n_estimators=1000)
+                model = clf.fit(tfidf_train_vectors, df_classes)
+                predicted_fever_1 = model.predict(tfidf_test_vectors_1)
+                predicted_fever_2 = model.predict(tfidf_test_vectors_2)
+                filename = '../model/TFIDF_xgb_model_process_{}_turn_{}_Clean_Space.pkl'.format(str(set + 1),
+                                                                                                str(process + 1))
+                pickle.dump(model, open(filename, 'wb'))
+                classification_score_test_1 = classification_report(df_test_1_classes, predicted_fever_1, digits=4)
+                precision_test_1 = classification_score_test_1.split('\n')[-2].split()[2:-1][0]
+                recall_test_1 = classification_score_test_1.split('\n')[-2].split()[2:-1][1]
+                f1_score_test_1 = classification_score_test_1.split('\n')[-2].split()[2:-1][2]
+
+                result.append(precision_test_1)
+                result.append(recall_test_1)
+                result.append(f1_score_test_1)
+
+                classification_score_test_2 = classification_report(df_test_2_classes, predicted_fever_2, digits=4)
+                precision_test_2 = classification_score_test_2.split('\n')[-2].split()[2:-1][0]
+                recall_test_2 = classification_score_test_2.split('\n')[-2].split()[2:-1][1]
+                f1_score_test_2 = classification_score_test_2.split('\n')[-2].split()[2:-1][2]
+
+                result.append(precision_test_2)
+                result.append(recall_test_2)
+                result.append(f1_score_test_2)
+                result = pd.DataFrame([result])
+                final_result = pd.concat([final_result, result], axis=0)
+                print('Set: {} and Process: {} done'.format(set+1, process+1))
+    final_result.to_csv('../../data/XGB_TFIDF_result.csv', index=False)
 
 
-            df_claim_encode = []
-            df_evidence_encode = []
-
-            X_test = X_test.values.tolist()
-            y_test = y_test.values.tolist()
-
-            for claim in df_claim:
-                temp_all_dim = nlp.encode(claim)
-                df_claim_encode.append(temp_all_dim)
-            df_claim_encode = apply_pca(df_claim_encode)
-
-            X_test_encode = []
-            for test_data in X_test:
-                temp_all_dim = nlp.encode(test_data)
-                X_test_encode.append(temp_all_dim)
-            X_test_encode = apply_pca(X_test_encode)
-
-            df_claim_evidence_encode = pd.DataFrame(df_claim_encode)
-            X_test_encode = pd.DataFrame(X_test_encode)
-
-
-
-            clf = GradientBoostingClassifier(learning_rate=0.01, n_estimators=1000)
-            print("Model fitting started ....")
-            model = clf.fit(df_claim_evidence_encode, df_class)
-
-            filename = '../model/sent_bert_xgb_model_process_{}_turn_{}'.format(str(proecess), str(turn))
-            pickle.dump(model, open(filename, 'wb'))
-
-            xgb_predictions = model.predict(X_test_encode)
-
-            precision_metric_micro = precision_score(y_test, xgb_predictions)
-            recall_metric_micro = recall_score(y_test, xgb_predictions)
-            accuracy_metric_micro = accuracy_score(y_test, xgb_predictions)
-            f1_metric_micro = f1_score(y_test, xgb_predictions)
-            classification_score = classification_report(y_test, xgb_predictions, digits=4)
-
-            if (os.path.exists(config.XGB_NORMALIZED_OUT_PUT_FILE)):
-                f = open(config.XGB_NORMALIZED_OUT_PUT_FILE, "a")
-                f.write('For Test 1 linguist enrichment using Sentence BERT 14 Jan')
-                f.write('\n\n')
-            else:
-                f = open(config.XGB_NORMALIZED_OUT_PUT_FILE, "a")
-            f.write('\n\n')
-            f.write('\n\n')
-            f.write('Turn: '+ str(turn))
-            f.write('\n\n')
-            f.write('\n')
-            f.write('   Process: '+ str(proecess))
-            f.write('\n\n')
-            f.write("precision_macro:" + str(precision_metric_micro))
-            f.write(" recall_macro: " + str(recall_metric_micro))
-            f.write('\n')
-            f.write(" Accuracy: " + str(accuracy_metric_micro))
-            f.write('\n')
-            f.write(" f1_macro: " + str(f1_metric_micro))
-            f.write('\n')
-            f.write(" Classification score: " + str(classification_score))
-            f.write('\n')
-            f.close()
-
-            print("precision_metric micro", precision_metric_micro)
-            print("recall_metric micro", recall_metric_micro)
-            print("accuracy_metric micro", accuracy_metric_micro)
-            print("f1_metric micro", f1_metric_micro)
-            print(classification_score)
-            print('Done Turn: {}, Process: {}'.format(turn, proecess))
+XGB()
